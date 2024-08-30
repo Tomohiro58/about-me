1905e80703853723ac73e130398294b3a11e8b55
arakiyutadesu <167841610+arakiyutadesu@users.noreply.github.com>
2024-08-30 12:52:27 +0900

    コミット

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('自己紹介サイトへようこそ！');
 });
+// ページが完全に読み込まれたら実行
+window.addEventListener('load', function() {
+    // ローディング画面を非表示にする
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'none';
+    // メインコンテンツを表示する
+    const content = document.getElementById('content');
+    content.style.display = 'block';
+ });
